6bd29783ee69470c0e730366d3366a26a001e534
Zaman <motaharz95@gmail.com>
2021-08-26 01:29:23 +0600

    learn to use django template language

--- testapp/views.py
+++ testapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello world")
+    return render(request, 'home.html', {'name' : 'Fariya'})
