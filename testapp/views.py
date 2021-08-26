d35d9213065f1a84453ffeb6e0ffd7d384974cd4
Zaman <motaharz95@gmail.com>
2021-08-26 10:02:37 +0600

    use of POST method and csrf token

--- testapp/views.py
+++ testapp/views.py
@@ -7,7 +7,7 @@ def home(request):
     return render(request, 'home.html', {'name' : 'Fariya'})
 
 def add(request):
-    no1 = int(request.GET['num1'])
-    no2 = int(request.GET['num2'])
+    no1 = int(request.POST['num1'])
+    no2 = int(request.POST['num2'])
     res = no1 + no2
     return render(request, 'result.html', {'result' : res})
