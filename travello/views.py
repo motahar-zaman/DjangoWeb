f3540ef759f45e86e33c622426c52fa5d8011f5e
Zaman <motaharz95@gmail.com>
2021-08-28 01:38:05 +0600

    use of Class, models and passing dynamic data(object) to UI

--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    dhaka = Destination()
+    dhaka.name = 'Dhaka'
+    dhaka.desc = 'dhaka is a beautiful city in Bangladesh'
+    dhaka.price = 7100
+
+    chittagong = Destination()
+    chittagong.name = 'Chittagong'
+    chittagong.desc = 'Chittagong is a beautiful business city in our country'
+    chittagong.price = 1100
+
+    jessore = Destination()
+    jessore.name = 'Jessore'
+    jessore.desc = 'Jessore is a historic city in Bangladesh'
+    jessore.price = 7900
+    return render(request, 'index.html', {'dhaka' : dhaka, 'chittagong' : chittagong, 'jessore' : jessore})
