0044ce71510dfcbeef3dc36a8d2d0f39fad43c2b
Zaman <motaharz95@gmail.com>
2021-08-28 12:12:26 +0600

    conditional(if) case in UI

--- travello/views.py
+++ travello/views.py
@@ -9,18 +9,21 @@ def index(request):
     dhaka.desc = 'dhaka is a beautiful city in Bangladesh'
     dhaka.img = 'destination_1.jpg'
     dhaka.price = 7100
+    dhaka.offer = False
 
     chittagong = Destination()
     chittagong.name = 'Chittagong'
     chittagong.desc = 'Chittagong is a beautiful business city in our country'
     chittagong.img = 'destination_2.jpg'
     chittagong.price = 1100
+    chittagong.offer = True
 
     jessore = Destination()
     jessore.name = 'Jessore'
     jessore.desc = 'Jessore is a historic city in Bangladesh'
     jessore.img = 'destination_3.jpg'
     jessore.price = 7900
+    jessore.offer = False
 
     dests = [dhaka, chittagong, jessore]
     return render(request, 'index.html', {'dests' : dests})
