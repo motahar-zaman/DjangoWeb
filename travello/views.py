6ebac1d8d3a2d17bd9fe72ccb9651d3958a574cd
Zaman <motaharz95@gmail.com>
2021-08-28 12:03:15 +0600

    print dynamic data(object) to UI

--- travello/views.py
+++ travello/views.py
@@ -7,15 +7,20 @@ def index(request):
     dhaka = Destination()
     dhaka.name = 'Dhaka'
     dhaka.desc = 'dhaka is a beautiful city in Bangladesh'
+    dhaka.img = 'destination_1.jpg'
     dhaka.price = 7100
 
     chittagong = Destination()
     chittagong.name = 'Chittagong'
     chittagong.desc = 'Chittagong is a beautiful business city in our country'
+    chittagong.img = 'destination_2.jpg'
     chittagong.price = 1100
 
     jessore = Destination()
     jessore.name = 'Jessore'
     jessore.desc = 'Jessore is a historic city in Bangladesh'
+    jessore.img = 'destination_3.jpg'
     jessore.price = 7900
-    return render(request, 'index.html', {'dhaka' : dhaka, 'chittagong' : chittagong, 'jessore' : jessore})
+
+    dests = [dhaka, chittagong, jessore]
+    return render(request, 'index.html', {'dests' : dests})
