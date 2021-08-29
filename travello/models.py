0a3d8c4f859eaa23bd8daee033109a92e1da1002
Zaman <motaharz95@gmail.com>
2021-08-29 13:30:19 +0600

    correct a field or add a field and re migration

--- travello/models.py
+++ travello/models.py
@@ -8,5 +8,5 @@ class Destination(models.Model):
     name = models.CharField(max_length = 100)
     img = models.ImageField(upload_to = 'images')
     desc = models.TextField()
-    price = models.IntegerField
+    price = models.IntegerField()
     offer = models.BooleanField(default = False)
